42c442bf6b05ef0565ee4125185129faf59d7b43
aristotle/ billie bust up <116121287+tiktokfurry123@users.noreply.github.com>
2024-05-21 13:28:05 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 input.onButtonPressed(Button.A, function () {
-    music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.LoopingInBackground)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
     basic.showIcon(IconNames.Happy)
 })
